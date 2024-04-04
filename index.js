7c25b8a93aa4036c4b8c893e9812d4ea135ccfcd
Reilly Shofner <reilly.shofner@gmail.com>
2024-04-04 16:49:11 -0700

    complete phase-1-array-filter-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+
+function findMatching(names, string) {
+return names.filter(function (name) {
+    return name.toLowerCase() === string.toLowerCase();
+});
+}
+
+function fuzzyMatch(driverArray, nameLetters) {
+  return driverArray.filter(function (name) {
+    return name.slice(0,2) === nameLetters;
+  });
+}
+
+function matchName(driverObjs, string) {
+return driverObjs.filter(function (matching){
+  return matching.name === string;
+}); 
+  
+}
